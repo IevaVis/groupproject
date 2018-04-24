2c5b78a039088740b8a95915a35521c1fadb0e4e
IevaVis <ieva.visniauskiene@gmail.com>
2018-04-24 19:02:24 +0800

    added bootstrap, jquery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < Clearance::UsersController
+
+	def new
+		@user = User.new
+		@role = get_role(params[:role])
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def allowed_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :about_me)
+	end
+
+
 end
